7d1255713683674e6cdf30fd07ec6f4186a24708
Matt Dias <matt.dias6@gmail.com>
2014-07-29 23:22:09 -0400

    Initial integration work of MDCSwipeToChoose. Too tired to continue, tonight.

---
+++ Cuteness/PostView.h
@@ -0,0 +1,13 @@
+//
+//  PostView.h
+//  Cuteness
+//
+//  Created by Matt Dias on 7/29/14.
+//  Copyright (c) 2014 saiDttaM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostView : UIView
+
+@end
